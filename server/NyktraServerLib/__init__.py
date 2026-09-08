d69b4fcd40529c8dc78156a4ea9a8f1039080fc4
T3CHN0 <abdoessam.999.t3chn0@gmail.com>
2026-09-08 04:45:39 +0300

    dev update

---
+++ server/NyktraServerLib/__init__.py
@@ -0,0 +1,5 @@
+from .cli_args import cli_args
+
+__version__ = "1.0.0"
+__author__ = "Abdelrahmen Essam"
+__all__ = ["cli_args"]
---
+++ server/NyktraServerLib/cli_args.py
@@ -0,0 +1,90 @@
+import click
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+
+console = Console()
+
+banner = Text("""
+███╗   ██╗██╗   ██╗██╗  ██╗████████╗██████╗  █████╗
+████╗  ██║╚██╗ ██╔╝██║ ██╔╝╚══██╔══╝██╔══██╗██╔══██╗
+██╔██╗ ██║ ╚████╔╝ █████╔╝    ██║   ██████╔╝███████║
+██║╚██╗██║  ╚██╔╝  ██╔═██╗    ██║   ██╔══██╗██╔══██║
+██║ ╚████║   ██║   ██║  ██╗   ██║   ██║  ██║██║  ██║
+╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝
+""", style="purple")
+
+def print_version(ctx, param, value):
+  if not value or ctx.resilient_parsing:
+    return
+
+  console.print()
+  console.print(
+    Panel(
+      banner,
+      title="[bold cyan]NYKTRA 1.0.0[/bold cyan]",
+      subtitle="[cyan]Secure • Fast • Terminal[/cyan]",
+      border_style="cyan"
+    ),
+    justify="left"
+  )
+  ctx.exit()
+
+def nor_print_version():
+  console.print()
+  console.print(
+    Panel(
+      banner,
+      title="[bold cyan]NYKTRA 1.0.0[/bold cyan]",
+      subtitle="[cyan]Secure • Fast • Terminal[/cyan]",
+      border_style="cyan"
+    ),
+    justify="left"
+  )
+  console.print()
+
+@click.group()
+@click.option(
+  "-v", "--version", 
+  is_flag=True, 
+  callback=print_version, 
+  expose_value=False, 
+  is_eager=True, 
+  help="Show the version and exit."
+)
+def cli_args(): pass
+
+@cli_args.command(
+  help="Run the server"
+)
+@click.option(
+  "-p", "--port",
+  type=int, default=77666,
+  help="Port number for the server to work on. example: 7000"
+)
+def run(port):
+  nor_print_version()
+  console.print("\n[bold green]\[+] Server is running")
+  console.print(f"[cyan]\[*] Listening on 127.0.0.1:{port}")
+
+@cli_args.command(
+  help="Configure the server settings to edit the config.json file"
+)
+@click.option(
+  "-v", "--variable",
+  help="Specify a config variable to edit"
+)
+def config(variable):
+  nor_print_version()
+  if variable:
+    console.print(f"[green]\[+] Edited: [purple]{variable}[/purple]")
+  else:
+    console.print(f"[green]\[+] Started config")
+
+@cli_args.command(
+  help="Add a group on the server"
+)
+@click.argument("group_name")
+def add(group_name):
+  nor_print_version()
+  console.print(f"[green]\[+] Added as group: [purple]{group_name}[/purple]")

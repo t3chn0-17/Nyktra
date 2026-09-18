defc33c1dcdaebbec4d43219385cc45b2c91eabc
T3CHN0 <abdoessam.999.t3chn0@gmail.com>
2026-09-18 09:28:10 +0300

    add: ensure dir folders existance

--- Nyktra-Curses/NykLib/args.py
+++ Nyktra-Curses/NykLib/args.py
@@ -3,7 +3,7 @@ from rich.panel import Panel
 from rich.text import Text
 
 from .utils import dev_banner, print_version, get_local_ip
-from .config import console
+from .config import console, ensure_dirs
 
 @click.group()
 @click.option(
@@ -26,6 +26,7 @@ def cli_args(): pass
 )
 def run(port):
   dev_banner()
+  ensure_dirs()
   console.print(f"[green]\[+] Server running on 127.0.0.1:{port}")
   console.print(f"[blue]\[!] Access it on the LAN {get_local_ip()}:{port}")
   console.print("[blue]\[!] Press Enter to Start the TUI")
--- Nyktra-Curses/NykLib/config.py
+++ Nyktra-Curses/NykLib/config.py
@@ -5,3 +5,7 @@ console = Console()
 
 db_dir = Path(__file__).parent.parent / "databases"
 sounds_dir = Path(__file__).parent.parent / "sounds"
+
+def ensure_dirs() -> None:
+  db_dir.mkdir(parents=True, exist_ok=True)
+  sounds_dir.mkdir(parents=True, exist_ok=True)

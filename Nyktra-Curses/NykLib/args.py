758aa1143aa15a2af39a5a44c33ba09af6ef092f
T3CHN0 <abdoessam.999.t3chn0@gmail.com>
2026-09-18 10:21:29 +0300

    add: server_args group and client_args group

--- Nyktra-Curses/NykLib/args.py
+++ Nyktra-Curses/NykLib/args.py
@@ -14,15 +14,49 @@ from .config import console, ensure_dirs
   is_eager=True, 
   help="Show the version and exit."
 )
-def cli_args(): pass
+def server_args(): pass
 
-@cli_args.command(
+@server_args.command(
   help="Run the server"
 )
 @click.option(
   "-p", "--port",
   type=int, default=77666,
   help="Port number for the server to work on. example: 7000"
+)
+def run(port):
+  dev_banner()
+  ensure_dirs()
+  # console.print(f"[green]\[+] Server running on 127.0.0.1:{port}")
+  # console.print(f"[blue]\[!] Access it on the LAN {get_local_ip()}:{port}")
+  console.print("[blue]\[!] Press Enter to Start the TUI")
+  input()
+
+
+
+@click.group()
+@click.option(
+  "-v", "--version", 
+  is_flag=True, 
+  callback=print_version, 
+  expose_value=False, 
+  is_eager=True, 
+  help="Show the version and exit."
+)
+def client_args(): pass
+
+@client_args.command(
+  help="Run the server"
+)
+@click.argument(
+  "ipaddr",
+  type=str,
+  help="IPv4 address of Nyktra server to connect to. example: 172.217.14.206"
+)
+@click.argument(
+  "port",
+  type=int,
+  help="Port number of Nyktra server to connect to. example: 7000"
 )
 def run(port):
   dev_banner()

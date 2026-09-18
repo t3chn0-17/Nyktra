0efef4af9ce3545f59a499971a73a3251db0c2f5
T3CHN0 <abdoessam.999.t3chn0@gmail.com>
2026-09-18 09:43:47 +0300

    add: initialize sounds function

--- Nyktra-Curses/NykLib/ui/sounds.py
+++ Nyktra-Curses/NykLib/ui/sounds.py
@@ -1,11 +1,14 @@
 from pygame import mixer
 from ..config import sounds_dir
 
-mixer.init()
+sounds = {}
 
-sounds = {
-  "click": mixer.Sound(sounds_dir / "click.wav"),
-  "select": mixer.Sound(sounds_dir / "select.wav"),
-  "error": mixer.Sound(sounds_dir / "error.wav"),
-  "message": mixer.Sound(sounds_dir / "message.wav"),
-}
+def init_sounds() -> None:
+  mixer.init()
+
+  sounds.update({
+    "click": mixer.Sound(sounds_dir / "click.wav"),
+    "select": mixer.Sound(sounds_dir / "select.wav"),
+    "error": mixer.Sound(sounds_dir / "error.wav"),
+    "message": mixer.Sound(sounds_dir / "message.wav"),
+  })
